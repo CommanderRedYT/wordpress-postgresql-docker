71a071e2f03b60d5447ae568f9bddd3be9c2368b
CommanderRedYT <ccommanderred@gmail.com>
2024-06-25 19:51:53 +0200

    Update entrypoint

--- docker-entrypoint-custom.sh
+++ docker-entrypoint-custom.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+VOLUME_VERSION="$(php -r 'require('"'"'wp-includes/version.php'"'"'); echo $wp_version;')"
+echo "Volume version: $VOLUME_VERSION"
+echo "Image version: $WORDPRESS_VERSION"
+
+if [ "$VOLUME_VERSION" != "$WORDPRESS_VERSION" ];
+then
+    echo "Version mismatch, copying files..."
+    rm -fv index.php
+fi
+
 mkdir -p "wp-content"
 if ! [ -e "wp-content/db.php" ];
 then
